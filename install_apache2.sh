f1362533a94e3e3e27733c743aa9564c62e8e28f
ElektroPac <Elektropac@gmail.com>
2023-09-13 07:55:53 +0200

    gf

---
+++ install_apache2.sh
@@ -0,0 +1,15 @@
+sudo apt install apache2 php
+
+sudo apt install mariadb-server
+
+sudo mysql_secure_installation
+
+
+sudo apt install phpmyadmin
+
+sudo dpkg-reconfigure phpmyadmin
+
+
+sudo systemctl status mysql
+
+mysql -u root -p
