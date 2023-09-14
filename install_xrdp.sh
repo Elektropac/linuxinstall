7743b9e99201373819a5096e059060c7eafe49ad
ElektroPac <43667224+Elektropac@users.noreply.github.com>
2023-09-14 10:11:20 +0200

    Update install_xrdp.sh

--- install_xrdp.sh
+++ install_xrdp.sh
@@ -15,14 +15,21 @@ sudo systemctl enable xrdp
 
 # Configure Raspberry Pi for remote access
 # Use raspi-config to enable VNC
-sudo raspi-config
+#sudo raspi-config
 
 # README section
-echo "----------------------------------------"
-echo "Script execution complete!"
-echo "To make this script executable, use the following command:"
-echo "chmod +x setup_xrdp.sh"
+#echo "----------------------------------------"
+#echo "Script execution complete!"
+#echo "To make this script executable, use the following command:"
+#echo "chmod +x setup_xrdp.sh"
 
-echo "To run the script, execute it with sudo privileges:"
-echo "sudo ./setup_xrdp.sh"
+#echo "To run the script, execute it with sudo privileges:"
+#echo "sudo ./setup_xrdp.sh"
+echo "----------------------------------------"
+
+echo "You need to make a new user, root account can not be used with xrdp for sequrety reasons"
+echo "From a terminal, shift to root account using  su <account>"
+echo "Now type sudo usermod -aG sudo <That account you wont to be user>"
+echo "You can now log on via xrdp using this new account"
+
 echo "----------------------------------------"
