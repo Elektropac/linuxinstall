e9a0debe4f3942b68689ad394c9b9ccd891b87d9
ElektroPac <43667224+Elektropac@users.noreply.github.com>
2023-09-14 10:21:45 +0200

    Update install_apache2.sh

--- install_apache2.sh
+++ install_apache2.sh
@@ -1,15 +1,28 @@
-sudo apt install apache2 php
+#!/bin/bash
 
-sudo apt install mariadb-server
+read -p "Do you want to install Apache and PHP? (y/n): " choice
+if [ "$choice" == "y" ]; then
+    sudo apt install apache2 php
+fi
 
-sudo mysql_secure_installation
+read -p "Do you want to install MariaDB server? (y/n): " choice
+if [ "$choice" == "y" ]; then
+    sudo apt install mariadb-server
+    sudo mysql_secure_installation
+fi
 
+read -p "Do you want to install phpMyAdmin? (y/n): " choice
+if [ "$choice" == "y" ]; then
+    sudo apt install phpmyadmin
+    sudo dpkg-reconfigure phpmyadmin
+fi
 
-sudo apt install phpmyadmin
+read -p "Do you want to check the status of MySQL? (y/n): " choice
+if [ "$choice" == "y" ]; then
+    sudo systemctl status mysql
+fi
 
-sudo dpkg-reconfigure phpmyadmin
-
-
-sudo systemctl status mysql
-
-mysql -u root -p
+read -p "Do you want to log in to MySQL as root? (y/n): " choice
+if [ "$choice" == "y" ]; then
+    mysql -u root -p
+fi
